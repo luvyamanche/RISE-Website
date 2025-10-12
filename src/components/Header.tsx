7a412e199e3223b950bbba8f10b2e3d86f1c5c59
Luvyakrish Manche <luvyakrishmanche@mac.lan>
2025-10-12 12:04:01 -0400

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,8 +28,8 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-3">
-            <div className="bg-blue-600 p-2 rounded-lg">
-              <Lightbulb className="h-6 w-6 text-white" />
+            <div className="flex items-center">
+              <img src='/logo.jpeg' className = 'h-10 w-auto object-contain'/>
             </div>
             <div>
               <h1 className="text-xl font-bold text-gray-900">RISE Foundation</h1>
